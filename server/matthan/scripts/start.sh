5e062d5faf81db6873b9a24141eeddc17fca402d
Dat Do <datdo@rjc.co.jp>
2016-06-02 01:49:45 +0900

    Prepare script to deploy real server

---
+++ server/matthan/scripts/start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+env=production
+dir=$(pwd)
+shims=/root/.rbenv/shims
+mkdir -p tmp/sockets
+mkdir -p tmp/pids
+$shims/puma config.ru                                     \
+  --tag         matthan.puma                                  \
+  --environment $env                                      \
+  --workers     2                                         \
+  --threads     1:16                                      \
+  --bind        unix:///$dir/tmp/sockets/matthan.puma.sock    \
+  --pidfile     tmp/pids/matthan.puma.pid                     \
+  --preload                                               \
+  --daemon
--- server/matthan/scripts/stop.sh
+++ server/matthan/scripts/stop.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-kill $(cat tmp/pids/thin.0.pid)
-kill $(cat tmp/pids/thin.1.pid)
-kill $(cat tmp/pids/thin.2.pid)
-
-service nginx stop
+kill $(cat tmp/pids/matthan.puma.pid)
